2eccd45e51e662b5b19690b49ca9a998fde5249c
Michael Pham N <michael.pham@cardcloset.com>
2020-04-17 08:01:06 -0700

    hw1

--- iris.py
+++ iris.py
@@ -9,6 +9,9 @@
 import pandas as pd
 from sklearn import tree
 from sklearn import metrics
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import KFold
+import numpy as np
 
 # Read the dataset into a dataframe and map the labels to numbers
 df = pd.read_csv('iris.csv')
@@ -24,4 +27,15 @@ y = df["label"]
 # Train a decision tree and compute its training accuracy
 clf = tree.DecisionTreeClassifier(max_depth=2, criterion='entropy')
 clf.fit(X, y)
-print(metrics.accuracy_score(y,clf.predict(X)))
+print(metrics.accuracy_score(y, clf.predict(X)))
+
+# Initialize the accuracy of the models to blank list. The accuracy of each model will be appended to this list
+accuracy_model = []
+
+kf = KFold(n_splits=10)
+for train_index, test_index in kf.split(X):
+    X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+    model = clf.fit(X_train, y_train)
+    accuracy_model.append(accuracy_score(y_test, model.predict(X_test)))
+print("Accuracy: ", np.mean(accuracy_model))
